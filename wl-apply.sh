45682df372a20c7c2b4de316b3305a2cdfc8d74e
daveweilert <daveweilert@gmail.com>
2025-04-22 18:30:48 -0500

    class files

---
+++ wl-apply.sh
@@ -0,0 +1,7 @@
+kubectl apply -f wl-pod.yaml
+kubectl apply -f wl-deployment.yaml
+kubectl apply -f wl-daemonset.yaml
+kubectl apply -f wl-replicaset.yaml
+kubectl apply -f wl-statefulset.yaml
+kubectl apply -f wl-job.yaml
+kubectl apply -f wl-cronjob.yaml
---
+++ wl-delete.sh
@@ -0,0 +1,7 @@
+kubectl delete -f wl-pod.yaml
+kubectl delete -f wl-deployment.yaml
+kubectl delete -f wl-daemonset.yaml
+kubectl delete -f wl-replicaset.yaml
+kubectl delete -f wl-statefulset.yaml
+kubectl delete -f wl-job.yaml
+kubectl delete -f wl-cronjob.yaml
